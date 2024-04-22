6fefd35f31fe44feee960cdff67913e27e646fa2
Tturna <eemelijuponaho@gmail.com>
2024-04-22 11:11:19 +0300

    maintainer registration, simple maintainer pages

---
+++ Controllers/MaintainerController.cs
@@ -0,0 +1,64 @@
+﻿using ASP_Ecommerce.Models;
+using ASP_Ecommerce.Models.Enums;
+using ASP_Ecommerce.Models.ViewModels;
+using ASP_Ecommerce.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_Ecommerce.Controllers;
+
+[Route("/maintainers")]
+public class MaintainerController(ApplicationDbContext dbContext, UserManager<UserModel> userManager) : Controller
+{
+    public IActionResult Index(int? id)
+    {
+        if (id == null)
+        {
+            var maintainers = dbContext.Users
+                .Where(u => u.MaintainerAddress != null)
+                .AsNoTracking()
+                .ToArray();
+            
+            return View(maintainers);
+        }
+        
+        var user = dbContext.Users.FirstOrDefault(u => u.Id == id);
+
+        if (user?.MaintainerAddress == null)
+        {
+            return NotFound();
+        }
+        
+        return View("Individual", user);
+    }
+
+    [Authorize, Route("/maintainers/register")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost, Authorize, ValidateAntiForgeryToken, Route("/maintainers/register")]
+    public IActionResult Register([FromForm] BecomeMaintainerViewModel newMaintainerData)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(newMaintainerData);
+        }
+        
+        // user can't be null because of the [Authorize] attribute
+        var user = dbContext.Users.First(u => u.UserName == User.Identity!.Name);
+
+        userManager.AddToRoleAsync(user, Enum.GetName(UserRole.Maintainer)!);
+        user.MaintainerAddress = newMaintainerData.MaintainerAddress;
+        user.City = newMaintainerData.City;
+        user.PostalCode = newMaintainerData.PostalCode;
+        user.Country = newMaintainerData.Country;
+            
+        dbContext.SaveChanges();
+        
+        return RedirectToAction("Index", new { id = user.Id });
+    }
+}
---
+++ Models/ViewModels/BecomeMaintainerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Ecommerce.Models.ViewModels;
+
+public class BecomeMaintainerViewModel
+{
+    [Required, StringLength(50)]
+    public string? MaintainerAddress { get; set; }
+    
+    [Required, StringLength(50)]
+    public string? City { get; set; }
+    
+    [Required, StringLength(50)]
+    public string? Country { get; set; }
+    
+    [Required, DataType(DataType.PostalCode)]
+    public int? PostalCode { get; set; }
+}
