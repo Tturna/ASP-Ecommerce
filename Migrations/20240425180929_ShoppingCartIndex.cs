6da0c8d382583bbb722408d235c2dda7d4c62007
Tturna <eemelijuponaho@gmail.com>
2024-04-25 21:10:57 +0300

    shopping cart index migration

---
+++ Migrations/20240425180929_ShoppingCartIndex.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP_Ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShoppingCartIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ShoppingCartId",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ShoppingCartId",
+                table: "AspNetUsers",
+                column: "ShoppingCartId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ShoppingCartId",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ShoppingCartId",
+                table: "AspNetUsers",
+                column: "ShoppingCartId");
+        }
+    }
+}
