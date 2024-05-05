e02ef63c07f8412d550aa85902370357f86cb6b6
Tturna <eemelijuponaho@gmail.com>
2024-05-05 16:07:48 +0300

    untested reviews

--- Models/ProductReviewModel.cs
+++ Models/ProductReviewModel.cs
@@ -7,17 +7,17 @@ public class ProductReviewModel
     public int Id { get; set; }
     
     [Required, StringLength(100)]
-    public required string Title { get; set; }
+    public string Title { get; set; }
     
     [Required, StringLength(500)]
-    public required string Content { get; set; }
+    public string Content { get; set; }
     
-    [Range(1, 5)]
-    public required int Rating { get; set; }
+    [Required, Range(1, 5)]
+    public int Rating { get; set; }
     
-    public required int ProductId { get; set; }
-    public required ProductModel Product { get; set; }
+    public int ProductId { get; set; }
+    public ProductModel Product { get; set; }
     
-    public required int UserId { get; set; }
-    public required UserModel User { get; set; }
+    public int UserId { get; set; }
+    public UserModel User { get; set; }
 }
